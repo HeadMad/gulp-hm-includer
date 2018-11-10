066d917e84a16f31708f6b093384d766b25817fa
HeadMad <kov.ps@ya.ru>
2018-11-10 16:42:59 +0800

    add patterns stack

--- config.js
+++ config.js
@@ -16,6 +16,116 @@ function writeProp (from, to, ...props) {
     });
 }
 
+/**
+ * Получаем массив паттернов
+ * @param {mix} pattern 
+ */
+function getPatterns( pattern ) {
+
+    const type = typeOf( pattern );
+
+    // если передан массив
+    if (type === 'Array') {
+        return pattern.reduce(function (stack, item) {
+            let pattern = transformPattern(item);
+            if ( pattern ) {
+                stack.push( pattern );
+            }
+            return stack;
+        }, []);
+    }
+
+    let result = transformPattern( pattern );
+
+    if (!result) {
+        return false;
+    }
+
+    return [result];
+}
+
+/**
+ * Трансформация паттерна
+ * @param {object|string} pattern 
+ */
+function transformPattern ( pattern ) {
+    const type = typeOf( pattern );
+
+    // если передана строка
+    if (type === 'String') {
+        try {
+            pattern = require('./patterns/' + pattern );
+            writeExpression( pattern );
+            return pattern;
+
+        } catch (error) {
+            logDanger('a non-existent pattern:', pattern );
+            return false;
+        }
+    }
+
+    // если передан не объект
+    if (type !== 'Object') {
+        return false;
+    }
+
+    // если не установлен паттерн или значение пути
+    if ( !pattern.expr || !pattern.path ) {
+        return false;
+    }
+
+    // если шаблон не регулярное выражение
+    if ( !pattern.expr instanceof RegExp ) {
+
+        // если шаблон и не строка
+        if (typeof pattern.expr !== 'string') {
+            logDanger('Invalid format of property:', 'pattern.path');
+            return false;
+        }
+
+        // если path не строка
+        if (typeof pattern.path !== 'string') {
+            logDanger('Invalid format of property:', 'pattern.path');
+            return false;
+        }
+
+        // если в паттерне нет ключевого слова
+        if (pattern.expr.indexOf(pattern.path) === -1) {
+            logDanger('pattern.path must be a substring of the pattern.expr');
+            return false;
+        }
+
+        // хеш для замены в выражении
+        let path = Date.now();
+
+        // переводим строку в регулярное выражение
+        let expr = pattern.expr.replace(pattern.path, path)
+            .replace(/\.|\^|\$|\*|\+|\?|\(|\)|\[|\]|\{|\}|\\|\|/g, '\\$&')
+            .replace(path, '(.+?)');
+
+        pattern.expr = new RegExp(expr, 'g');
+        pattern.path = '$1';
+
+    }
+
+    writeExpression( pattern );
+    return pattern;
+
+} // transformPattern
+
+
+
+/**
+ * Запись всех необходимых шаблонов в объект
+ * @param {object} pattern 
+ */
+function writeExpression( pattern ) {
+    const expr = pattern.expr;
+    pattern.gmExpr = new RegExp('^\\n?(([^\\n\\S]*).*?' + expr.source + '.*)$', 'gm');
+    pattern.expr = new RegExp(expr.source);
+    pattern.gExpr = expr;
+}
+
 const Config = {
 
     import: function (params) {
@@ -28,76 +138,15 @@ const Config = {
 
     },
 
-    writeExpression ( expr ) {
-        this.gmExpr = new RegExp( '^\\n?(([^\\n\\S]*).*?' + expr.source + '.*)$', 'gm' );
-        this.expr = new RegExp( expr.source );
-        this.gExpr = expr;
-    },
 
-    set pattern(value) {
 
-        let type = typeOf( value );
-
-        // если передана строка
-        if (type === 'String') {
-            try {
-                let pattern = require('./patterns/' + value);
-                this.writeExpression(pattern.expr, pattern.path);
-                writeProp( pattern, this, 'path', 'wrap' );
-            } catch (error) {
-                logDanger('a non-existent pattern:', value);
-            }
-            return;
-        }
-        
-        
-        if (type !== 'Object')
-        return;
-        
-        // если не установлен паттерн или значение пути
-        if (!value.expr || !value.path)
-        return;
-
-        writeProp( value, this, 'wrap' );
-
-        
-        // если регулярное выражение
-        if (value.expr instanceof RegExp) {
-            this.writeExpression(value.expr);
-            writeProp( value, this, 'path' );
-            return;
-        }
-        
-        // если не строка
-        if (typeof value.expr !== 'string') {
-            logDanger('Invalid format of property:', 'pattern.expr');
-            return;
+    set pattern( value ) {
+        const patterns = getPatterns( value );
+        if ( patterns ) {
+            this.patterns = patterns;
         }
 
-        // если path не строка
-        if (typeof value.path !== 'string') {
-            logDanger('Invalid format of property:', 'pattern.path');
-            return;
-        }
-        
-        // если в паттерне нет ключевого слова
-        if (value.expr.indexOf(value.path) === -1) {
-            logDanger('pattern.path must be a substring of the pattern.expr');
-            return;
-        }
-
-        // хеш для замены в выражении
-        let path = Date.now();
-
-        // переводим строку в регулярное выражение
-        let expr = value.expr.replace(value.path, path)
-                             .replace(/\.|\^|\$|\*|\+|\?|\(|\)|\[|\]|\{|\}|\\|\|/g, '\\$&')
-                             .replace(path, '(.+?)');
-
-        this.writeExpression(new RegExp(expr, 'g'));
-
-        writeProp( {path: '$1'}, this, 'path');
-
+        console.log(patterns);
     },
 
 };
--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ const include = require('./index.js');
 
 gulp.task('default', function () {
     return gulp.src('./test/build.html')
-               .pipe(include({pattern: 'javascript-include'}))
-               .pipe(include({pattern: 'css-include'}))
-               .pipe(include({pattern: 'html-import'}))
-               .pipe(include({pattern: 'css-import'}))
+               .pipe(include({pattern:[
+                   'javascript-inline',
+                   'css-inline',
+                   'html-import',
+                   'css-import'
+                ]}))
                .pipe(gulp.dest('./'))
 });
 
